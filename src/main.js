438db2f947b0ad627d88a9ce49179881310b0479
Stas <botev.st@gmail.com>
2017-08-25 17:47:19 +0600

    Переписано

--- src/main.js
+++ src/main.js
@@ -1,151 +1,183 @@
-import Vue from 'vue'
-import VueAxios from 'vue-axios'
-import Axios from 'axios'
-
-Vue.use(VueAxios, Axios)
-
 const app = new Vue({
-    el: '#app',
-    data: {
-        items: [],
-        data: {},
-        query: ''
-    },
-    computed: {
-        filteredItems:function() {
-        var self=this;
-        return this.items.filter(function(item){return item.name.toLowerCase().indexOf(self.query.toLowerCase())>=0;}); }
-    },
-    mounted() {
-        self = this;
-        this.$http.get(
-            "https://api.hh.ru/vacancies?text=javascript+css+OR+jquery&area=160&per_page=500&order_by=publication_time"
-        ).then(function (response) {
-            self.items = response.data.items;
-            self.data = response.data;
-            console.log(response);
-        }).catch(function (error) {
-            console.log(error);
-        });
-    },
-    methods: {
-        isDesign: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/дизайн/i || /UI/i || /UX/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isNode: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/Node/i || /node/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isMongo: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/Mongo/i || /mongo/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isPHP: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/php/i || /PHP/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isHTML: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/html/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isCSS: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/css/i || /CSS/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isSQL: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/sql/i || /SQL/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isJS: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/javascript/ig || /js/ig || /script/ig) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isJQ: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/jquery/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isVue: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/vue/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isAngular: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/angular/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isReact: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/react/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isTS: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/typescript/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isPython: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/python/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        isBootstrap: (item) => {
-            var str = item.snippet.requirement + item.snippet.responsibility + item.name;
-            if (str.search(/bootstrap/i) == -1) {
-                return false;
-            }
-            return true;
-        },
-        map: (item) => {
-            return item.address === null ? '' : 'https://yandex.kz/maps/162/almaty/?ll=' + item.address.lng + '%2C' + item.address.lat + '&z=18&mode=whatshere&whatshere%5Bpoint%5D=' + item.address.lng + '%2C' + item.address.lat + '&whatshere%5Bzoom%5D=18';
-        },
-        salary: (item) => {
-            if (item.salary === null) {
-                return 'не указана';
-            }
-            var salary = item.salary.to === null ? '' : ' до ' + item.salary.to;
-            return ' от ' + item.salary.from + salary + ' ' + item.salary.currency;
-        },
-        city: (item) => {
-            return item.address === null ? 'не указан' : item.address.street + ' ' + (item.address.building === null ? '' : item.address.building);
-        }
+  el: "#app",
+  data: {
+    items: [],
+    data: {},
+    query: ""
+  },
+  computed: {
+    filteredItems: function() {
+      var self = this;
+      return this.items.filter(function(item) {
+        return item.name.toLowerCase().indexOf(self.query.toLowerCase()) >= 0;
+      });
     }
+  },
+  mounted() {
+    self = this;
+    this.$http
+      .get(
+        "https://api.hh.ru/vacancies?text=javascript&area=160&per_page=500&order_by=publication_time"
+      )
+      .then(function(response) {
+        self.items = response.data.items;
+        self.data = response.data;
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  },
+  methods: {
+    isDesign: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/дизайн/i || /UI/i || /UX/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isNode: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/Node/i || /node/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isMongo: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/Mongo/i || /mongo/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isPHP: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/php/i || /PHP/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isHTML: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/html/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isCSS: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/css/i || /CSS/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isSQL: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/sql/i || /SQL/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isJS: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/javascript/gi || /js/gi || /script/gi) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isJQ: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/jquery/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isVue: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/vue/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isAngular: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/angular/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isReact: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/react/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isTS: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/typescript/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isPython: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/python/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    isBootstrap: item => {
+      var str =
+        item.snippet.requirement + item.snippet.responsibility + item.name;
+      if (str.search(/bootstrap/i) == -1) {
+        return false;
+      }
+      return true;
+    },
+    map: item => {
+      return item.address === null
+        ? ""
+        : "https://yandex.kz/maps/162/almaty/?ll=" +
+          item.address.lng +
+          "%2C" +
+          item.address.lat +
+          "&z=18&mode=whatshere&whatshere%5Bpoint%5D=" +
+          item.address.lng +
+          "%2C" +
+          item.address.lat +
+          "&whatshere%5Bzoom%5D=18";
+    },
+    salary: item => {
+      if (item.salary === null) {
+        return "не указана";
+      }
+      var salary =
+        item.salary.to === null
+          ? ""
+          : " до " + Math.floor(item.salary.to / 5.6);
+      return " от " + Math.floor(item.salary.from / 5.6) + salary + " " + "RUB";
+    },
+    city: item => {
+      return item.address === null
+        ? "не указан"
+        : item.address.street +
+          " " +
+          (item.address.building === null ? "" : item.address.building);
+    }
+  }
 });
