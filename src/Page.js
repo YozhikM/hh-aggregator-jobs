dfd685d5e42b0db254ee286e2af810cdf03e7793
YozhikM <botev.st@gmail.com>
2018-01-15 21:43:54 +0600

    feat(Page): add badges

--- src/Page.js
+++ src/Page.js
@@ -4,7 +4,6 @@ import * as React from 'react';
 import 'papercss/dist/paper.min.css';
 import { type Jobs } from './type';
 
-
 type Props = {
   jobs?: Jobs,
 };
@@ -13,17 +12,47 @@ type Salary = ?{|
   +to: ?number,
   +gross: boolean,
   +from: ?number,
-  +currency: string
+  +currency: string,
 |};
 
+const badges = [
+  { name: 'react', color: 'success' },
+  { name: 'javascript', color: 'secondary' },
+  { name: 'vue', color: 'secondary' },
+  { name: 'angular', color: 'secondary' },
+  { name: 'jquery', color: 'warning' },
+  { name: 'php', color: 'danger' },
+  { name: 'junior', color: 'success' },
+];
+
 export default class Page extends React.Component<Props, void> {
-  getSalary(salary: Salary) {
+  getSalary(salary: Salary): string {
     const { from, to, currency } = salary || {};
 
     if (from && to && currency) return `от ${from} до ${to} ${currency}`;
     if (from && !to && currency) return `от ${from} ${currency}`;
     if (!from && to && currency) return `${to} ${currency}`;
     return `не указана`;
+  }
+
+  getBadges(string: string) {
+    const arr = badges
+      .map(badge => {
+        const reg = new RegExp(badge.name, 'gi');
+        if (string.match(reg)) return badge;
+      })
+      .filter(Boolean);
+    if (arr.length > 0) {
+      return (
+        <div>
+          {arr.map(a => (
+            <span key={a.name} style={{ marginRight: '10px' }} className={`badge ${a.color}`}>
+              {a.name}
+            </span>
+          ))}
+        </div>
+      );
+    }
   }
 
   render() {
@@ -42,6 +71,7 @@ export default class Page extends React.Component<Props, void> {
             <div className="sm-1 md-3 lg-4 col align-top" key={id}>
               <div className="card">
                 <div className="card-header">
+                  {this.getBadges(name + requirement + (responsibility || ''))}
                   <h4 className="card-subtitle" style={{ fontFamily: '"Neucha",sans-serif' }}>
                     {name}
                   </h4>
