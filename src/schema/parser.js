2b8a31c4664d2d6c0992ce0a4dd46d2ebac8665a
YozhikM <botev.st@gmail.com>
2018-03-13 15:00:21 +0600

    refactor: replace logic

--- src/schema/parser.js
+++ src/schema/parser.js
@@ -1,8 +1,90 @@
 /* @flow */
 
-const { parseToDB } = require('.');
+const fetch = require('node-fetch');
+const chalk = require('chalk');
 const initDB = require('../mongo');
+const { Job } = require('.');
 
 initDB();
+
+async function upsertDesc(job) {
+  const { id } = job || {};
+
+  const response = await fetch(`https://api.hh.ru/vacancies/${id}`);
+  const result = await response.json();
+
+  return result.description;
+}
+
+async function upsertToDB(job, area) {
+  const description = await upsertDesc(job);
+
+  const newJob = new Job({ ...job, area, description });
+
+  await Job.insertMany(newJob, err => {
+    if (err) console.error(err);
+  });
+
+  return job;
+}
+
+async function getFetchData(page, area) {
+  const response = await fetch(
+    `https://api.hh.ru/vacancies?text=frontend+OR+node+OR+javascript&area=${area}&per_page=10&page=${page}&order_by=publication_time`
+  );
+  const result = await response.json();
+
+  return result;
+}
+
+const areas = [
+  { id: 160, value: 'Almaty' },
+  { id: 3, value: 'Yekaterinburg' },
+  { id: 4, value: 'Novosibirsk' },
+  { id: 88, value: 'Kazan' },
+  { id: 66, value: 'Nizhny Novgorod' },
+  { id: 76, value: 'Rostov-on-Don' },
+  { id: 1255, value: 'Tomsk' },
+  { id: 1438, value: 'Krasnodar' },
+  { id: 1586, value: 'Samara' },
+  { id: 2114, value: 'Crimea' },
+  { id: 2019, value: 'for the Moscow Ring Road' },
+];
+
+async function parseToDB() {
+  areas.forEach(async area => {
+    const { id, value } = area || {};
+    const logs = [];
+
+    let page = 1;
+    const result = [];
+    const fetchData = await getFetchData(page, id);
+    page += 1;
+    result.push(...fetchData.items);
+
+    if (fetchData.pages >= page) {
+      for (let i = 0; i < fetchData.pages; i++) {
+        // eslint-disable-next-line no-await-in-loop
+        const data = await getFetchData(page, id);
+        result.push(...data.items);
+        page += 1;
+      }
+    }
+
+    if (!result) return;
+
+    for (let i = 0; i < result.length; i++) {
+      // eslint-disable-next-line no-await-in-loop
+      const isRepeat = await Job.findOne({ id: result[i].id }).exec();
+      if (!isRepeat) {
+        // eslint-disable-next-line no-await-in-loop
+        const upsertedJob = await upsertToDB(result[i], id);
+        logs.push(upsertedJob);
+      }
+    }
+
+    console.log(chalk.magenta(`Recorded ${logs.length} for ${value}`));
+  });
+}
 
 parseToDB();
